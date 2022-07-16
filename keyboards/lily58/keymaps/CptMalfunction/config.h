e00207c423520cf9cc48e427e2f86ab55079e9f1
CptMalfunction <jan709.psn@gmail.com>
2022-07-16 12:33:19 +0200

    New layer for lily58 with rgb pattern and eeprom l/r

---
+++ keyboards/lily58/keymaps/CptMalfunction/config.h
@@ -0,0 +1,53 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+
+//#define MASTER_LEFT
+// #define MASTER_RIGHT
+#define EE_HANDS
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
+
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+
+#undef RGBLED_NUM
+#define RGBLED_NUM 20
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 20
+#define RGBLIGHT_SAT_STEP 34
+#define RGBLIGHT_VAL_STEP 34
+
+#undef RGB_DI_PIN
+#define RGB_DI_PIN D4
+#define RGBLED_SPLIT { 10, 10 }
+
+#define ENCODERS_PAD_A { F4 }
+#define ENCODERS_PAD_B { F5 }
+#define ENCODERS_PAD_A_RIGHT { F5 }
+#define ENCODERS_PAD_B_RIGHT { F4 }
+#define ENCODER_RESOLUTION 2
+
+#define OLED_DISPLAY_128X64
+#define SPLIT_OLED_ENABLE
