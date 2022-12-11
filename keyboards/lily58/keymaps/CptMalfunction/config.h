2cbd78fb79992686abd301af2da0ea2f8938a627
CptMalfunction <jan709.psn@gmail.com>
2022-12-11 14:12:43 +0100

    home row mods

--- keyboards/lily58/keymaps/CptMalfunction/config.h
+++ keyboards/lily58/keymaps/CptMalfunction/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 #define TAPPING_TOGGLE 1
 
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
